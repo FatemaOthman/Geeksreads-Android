991508c4f4c6a953725a7fe914d923860943768e
ShroukRashwan <shroukrashwan@gmail.com>
2019-03-16 16:22:34 +0200

    Group2 Version 1 Committed

---
+++ app/src/main/java/com/example/geeksreads/BookActivity.java
@@ -0,0 +1,64 @@
+package com.example.geeksreads;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
+
+
+public class BookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+        MenuItem item = menu.findItem(R.id.menuSearch);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setIconifiedByDefault(false);
+        searchView.setMaxWidth(800);
+        searchView.setQueryHint("Search books");
+        searchView.setBackgroundColor(getResources().getColor(R.color.white));
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    /*private class GetImage extends AsyncTask<String, Void, Bitmap> {
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            try {
+                URL url = new URL("http://xxx.xxx.xxx/image.jpg");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                return myBitmap;
+            }catch (Exception e){
+                // Log.d(TAG,e.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            //ImageView imageView = (ImageView) findViewById();
+            //imageView.setImageBitmap(result);
+        }
+    }*/
+}
+
---
+++ app/src/main/java/com/example/geeksreads/ReadBooksActivity.java
@@ -0,0 +1,132 @@
+package com.example.geeksreads;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ReadBooksActivity extends AppCompatActivity {
+
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    JSONArray data = new JSONArray();
+    Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_read_books);
+        mContext=this;
+
+        final String webService = "";
+
+        mSwipeRefreshLayout = findViewById(R.id.swipelayout);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                RequestTask task = new RequestTask();
+                task.execute(webService);
+            }
+        });
+
+        RequestTask task = new RequestTask();
+        task.execute(webService);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle("Read");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.setting_menu, menu);
+        MenuItem item = menu.findItem(R.id.menuSetting);
+        SearchView searchView = (SearchView) item.getActionView();
+        searchView.setMaxWidth(800);
+        searchView.setQueryHint("Search books");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public class RequestTask extends AsyncTask<String, Void, String> {
+        public static final String REQUEST_METHOD = "GET";
+        public static final int READ_TIMEOUT = 3000;
+        public static final int CONNECTION_TIMEOUT = 3000;
+        public Boolean internetConn = false;
+
+        @Override
+        protected String doInBackground(String... params){
+            String stringUrl = params[0];
+            String result;
+            String inputLine;
+            try {
+                //Create a URL object holding our url
+                URL myUrl = new URL(stringUrl);
+                //Create a connection
+                HttpURLConnection connection =(HttpURLConnection)
+                        myUrl.openConnection();
+                //Set methods and timeouts
+                connection.setRequestMethod(REQUEST_METHOD);
+                connection.setReadTimeout(READ_TIMEOUT);
+                connection.setConnectTimeout(CONNECTION_TIMEOUT);
+
+                //Connect to our url
+                connection.connect();
+                //Create a new InputStreamReader
+                InputStreamReader streamReader = new
+                        InputStreamReader(connection.getInputStream());
+                //Create a new buffered reader and String Builder
+                BufferedReader reader = new BufferedReader(streamReader);
+                StringBuilder stringBuilder = new StringBuilder();
+                //Check if the line we are reading is not null
+                while((inputLine = reader.readLine()) != null){
+                    stringBuilder.append(inputLine);
+                }
+                //Close our InputStream and Buffered reader
+                reader.close();
+                streamReader.close();
+                //Set our result equal to our stringBuilder
+                result = stringBuilder.toString();
+            }
+            catch(IOException e){
+                e.printStackTrace();
+                result = null;
+            }
+            return result;
+        }
+        protected void onPostExecute(String result){
+            mSwipeRefreshLayout.setRefreshing(false);
+            if(result==null) {
+                Toast.makeText(mContext,"Unable to connect to server", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            try {
+                ListView itemsList = findViewById(R.id.itemslist);
+                itemsList.setAdapter(new BookList_JSONAdapter(mContext, new JSONArray(result)));
+            }
+            catch(JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
