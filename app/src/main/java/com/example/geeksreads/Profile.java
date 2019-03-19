835d9d80aa2cf2aaeb8752dc852d0a41a87ee2f7
Amr Khaled <Amr.Soliman97@eng-st.cu.edu.eg>
2019-03-19 19:55:59 +0200

    Added My Profile Java class:
    -Implemented the user profile class.
    -Designed a sample layout for Followers and Follwoing in seoerate views.

--- app/src/main/java/com/example/geeksreads/Profile.java
+++ app/src/main/java/com/example/geeksreads/Profile.java
@@ -1,11 +1,12 @@
 package com.example.geeksreads;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Profile extends AppCompatActivity
 {
@@ -14,6 +15,44 @@ public class Profile extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
 
+
+
+        TextView Followers= findViewById(R.id.ActualFollowersCount);
+        Followers.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Profile.this,Followers.class);
+                startActivity(myIntent);
+            }
+        });
+
+        TextView Following= findViewById(R.id.ActualFollowingCount);
+        Following.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(Profile.this,Following.class);
+                startActivity(myIntent);
+            }
+        });
+
+
+        //////////////////////////////////////////////////////
+        //Strings and Variables:
+
+        String ActualFollowers = null; //Will be initialized from the database using the api.
+        String ActualFollowing = null;
+        int ImageIndex =1; //Dummy value that will be changed.
+
+        //Get Number of Followers and Following:
+        Followers.setText(ActualFollowers);
+        Following.setText(ActualFollowing);
+
+
+
+
+
+
+        //////////////////////////////////////////////////////
         Toolbar myToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setTitle("Profile");
@@ -21,6 +60,9 @@ public class Profile extends AppCompatActivity
 
 
 
-
+        ImageView UserProfilePicture = findViewById(R.id.UserProfilePhoto);
+        UserProfilePicture.setImageResource(ImageIndex);
     }
+
+
 }
