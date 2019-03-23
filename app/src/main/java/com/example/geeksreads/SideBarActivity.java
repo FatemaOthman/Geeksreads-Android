459e1dd98198c7bea88f83694049d7794cb48bd0
Fatema_Wahdan <fatema.othman98@gmail.com>
2019-03-23 19:13:12 +0200

    Sidebar Mimic service done

--- app/src/main/java/com/example/geeksreads/SideBarActivity.java
+++ app/src/main/java/com/example/geeksreads/SideBarActivity.java
@@ -1,5 +1,11 @@
 package com.example.geeksreads;
 
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,9 +18,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class SideBarActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    ImageView userPhoto;
+    TextView userName;
+    Context mContext;
+    TextView followersCount;
+    TextView booksCount;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +52,18 @@ public class SideBarActivity extends AppCompatActivity
         setContentView(R.layout.activity_side_bar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mContext = this;
+        userPhoto=findViewById(R.id.UserPhoto);
+        userName=findViewById(R.id.UserNameTxt);
+        followersCount=findViewById(R.id.Followers);
+        booksCount=findViewById(R.id.MyBooks);
+        JSONObject JSON = new JSONObject();
+
+        String UrlService = "http://geeksreads.000webhostapp.com/Fatema/SideBar.php";
+        GetSideBarDetails getSideBarDetails = new GetSideBarDetails();
+        getSideBarDetails.execute(UrlService,JSON.toString());
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -92,4 +134,117 @@ public class SideBarActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    private class GetUserPicture extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            try {
+                String photoUrl = params[0];
+                URL url = new URL(photoUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                return myBitmap;
+            }catch (Exception e){
+                // Log.d(TAG,e.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            userPhoto.setImageBitmap(result);
+
+        }
+    }
+
+    public class GetSideBarDetails extends AsyncTask<String, Void, String> {
+        public static final String REQUEST_METHOD = "GET";
+        //public static final int READ_TIMEOUT = 3000;
+        //public static final int CONNECTION_TIMEOUT = 3000;
+        AlertDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            dialog = new AlertDialog.Builder(mContext).create();
+            dialog.setTitle("Connection Status");
+        }
+
+        @Override
+        protected String doInBackground(String... params){
+            String UrlString = params[0];
+            String JSONString = params[1];
+            String result= "";
+
+            try {
+                //Create a URL object holding our url
+                URL url = new URL(UrlString);
+                HttpURLConnection http = (HttpURLConnection) url.openConnection();
+                http.setRequestMethod(REQUEST_METHOD);
+                http.setDoInput(true);
+                http.setDoOutput(true);
+
+                OutputStream ops = http.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(ops,"UTF-8"));
+                String data = URLEncoder.encode("id","UTF-8")+"="+URLEncoder.encode(JSONString,"UTF-8");
+
+                writer.write(data);
+                writer.flush();
+                writer.close();
+                ops.close();
+
+                //Create a new InputStreamReader
+                InputStream ips = http.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(ips,"ISO-8859-1"));
+                String line ="";
+                while ((line = reader.readLine()) != null)
+                {
+                    result += line;
+                }
+                reader.close();
+                ips.close();
+                http.disconnect();
+                return result;
+
+            } catch (MalformedURLException e) {
+                result = e.getMessage();
+            } catch (IOException e) {
+                result = e.getMessage();
+            }
+            return result;
+        }
+
+        @SuppressLint("SetTextI18n")
+        protected void onPostExecute(String result){
+            if(result==null) {
+                Toast.makeText(mContext,"Unable to connect to server", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            try {
+                dialog.setMessage("Done");
+                //dialog.show();
+
+                // TODO: Add your Post Execute logic here.
+                JSONObject jsonObject = new JSONObject(result);
+                followersCount.setText("Followers                                  "+jsonObject.getString("Followers"));
+                booksCount.setText("My Books                                  "+jsonObject.getString("CountBooks"));
+                userName.setText(jsonObject.getString("UserName"));
+                SideBarActivity.GetUserPicture Pic = new SideBarActivity.GetUserPicture();
+                Pic.execute(jsonObject.getString("photourl"));
+
+            }
+            catch(JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
+
+
+
 }
