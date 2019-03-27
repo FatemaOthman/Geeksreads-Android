5d64505b1a433e9d4f1307f473f69d5087df60e0
ShroukRashwan <shroukrashwan@gmail.com>
2019-03-27 11:59:57 +0200

    Added SideBar to Notification Activity.

--- app/src/main/java/com/example/geeksreads/NotificationActivity.java
+++ app/src/main/java/com/example/geeksreads/NotificationActivity.java
@@ -3,6 +3,7 @@ package com.example.geeksreads;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
@@ -18,8 +19,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.json.JSONArray;
@@ -37,15 +40,26 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class NotificationActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     Context mContext;
 
+    /* SideBar Views */
+    ImageView userPhoto;
+    TextView userName;
+    TextView followersCount;
+    TextView booksCount;
+    MenuItem FollowItem;
+    MenuItem BookItem;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification);
+        mContext = this;
 
         /* ToolBar and SideBar Setups */
         Toolbar myToolbar = findViewById(R.id.toolbar);
@@ -57,10 +71,41 @@ public class NotificationActivity extends AppCompatActivity implements Navigatio
         toggle.syncState();
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        /* Getting All views by id from SideBar Layout */
+        Menu menu = navigationView.getMenu();
+        MenuItem itemFollower = menu.findItem(R.id.Followers);
+        followersCount = (TextView) itemFollower.getActionView();
+        FollowItem = menu.findItem(R.id.Followers);
+        followersCount.setTextColor(getResources().getColor(R.color.white));
+        MenuItem itemBook = menu.findItem(R.id.MyBooks);
+        booksCount = (TextView) itemBook.getActionView();
+        booksCount.setTextColor(getResources().getColor(R.color.white));
+        BookItem = menu.findItem(R.id.MyBooks);
+        /* Get Header Items */
+        View mHeader = navigationView.getHeaderView(0);
+        userName = mHeader.findViewById(R.id.UserNameTxt);
+        userPhoto = mHeader.findViewById(R.id.UserPhoto);
+        userName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(NotificationActivity.this, Profile.class);
+                startActivity(mIntent);
+            }
+        });
+        userPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(NotificationActivity.this, Profile.class);
+                startActivity(mIntent);
+            }
+        });
+        JSONObject JSON = new JSONObject();
+        String UrlSideBar = "http://geeksreads.000webhostapp.com/Fatema/SideBar.php";
+        GetSideBarDetails getSideBarDetails = new GetSideBarDetails();
+        getSideBarDetails.execute(UrlSideBar,JSON.toString());
 
-        mContext = this;
 
-        final JSONObject JSON = new JSONObject();
+        final JSONObject jsonObject = new JSONObject();
         try {
             JSON.put("id", "value");
         }catch (JSONException e) {
@@ -79,7 +124,7 @@ public class NotificationActivity extends AppCompatActivity implements Navigatio
                             @Override
                             public void run() {
                                 GetNotificationsList performBackgroundTask = new GetNotificationsList();
-                                performBackgroundTask.execute(UrlService,JSON.toString());
+                                performBackgroundTask.execute(UrlService,jsonObject.toString());
                             }
                         });
                     }
@@ -114,7 +159,25 @@ public class NotificationActivity extends AppCompatActivity implements Navigatio
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        return false;
+
+        int id = menuItem.getItemId();
+
+        if (id == R.id.Home) {
+
+            Intent myIntent = new Intent(NotificationActivity.this, SideBarActivity.class);
+            startActivity(myIntent);
+
+        } else if (id == R.id.Followers) {
+            Intent myIntent = new Intent(NotificationActivity.this, FollowActivity.class);
+            startActivity(myIntent);
+
+        } else if (id == R.id.MyBooks) {
+            Intent myIntent = new Intent(NotificationActivity.this, BookActivity.class);
+            startActivity(myIntent);
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     /**
@@ -198,4 +261,118 @@ public class NotificationActivity extends AppCompatActivity implements Navigatio
         }
 
     }
+
+
+    /**
+     * Class that get sidebar profile pic. from server
+     */
+    private class GetUserPicture extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... params) {
+            try {
+                String photoUrl = params[0];
+                URL url = new URL(photoUrl);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setDoInput(true);
+                connection.connect();
+                InputStream input = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(input);
+                return myBitmap;
+            }catch (Exception e){
+                // Log.d(TAG,e.getMessage());
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap result) {
+            userPhoto.setImageBitmap(result);
+
+        }
+    }
+
+    /**
+     * Class that get sidebar Data from server
+     */
+    private class GetSideBarDetails extends AsyncTask<String, Void, String> {
+        static final String REQUEST_METHOD = "GET";
+        //public static final int READ_TIMEOUT = 3000;
+        //public static final int CONNECTION_TIMEOUT = 3000;
+        AlertDialog dialog;
+
+        @Override
+        protected void onPreExecute() {
+            dialog = new AlertDialog.Builder(mContext).create();
+            dialog.setTitle("Connection Status");
+        }
+
+        @Override
+        protected String doInBackground(String... params){
+            String UrlString = params[0];
+            String JSONString = params[1];
+            String result= "";
+
+            try {
+                //Create a URL object holding our url
+                URL url = new URL(UrlString);
+                HttpURLConnection http = (HttpURLConnection) url.openConnection();
+                http.setRequestMethod(REQUEST_METHOD);
+                http.setDoInput(true);
+                http.setDoOutput(true);
+
+                OutputStream ops = http.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(ops, StandardCharsets.UTF_8));
+                String data = URLEncoder.encode("id","UTF-8")+"="+URLEncoder.encode(JSONString,"UTF-8");
+
+                writer.write(data);
+                writer.flush();
+                writer.close();
+                ops.close();
+
+                //Create a new InputStreamReader
+                InputStream ips = http.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(ips, StandardCharsets.ISO_8859_1));
+                String line ="";
+                while ((line = reader.readLine()) != null)
+                {
+                    result += line;
+                }
+                reader.close();
+                ips.close();
+                http.disconnect();
+                return result;
+
+            } catch (MalformedURLException e) {
+                result = e.getMessage();
+            } catch (IOException e) {
+                result = e.getMessage();
+            }
+            return result;
+        }
+
+        @SuppressLint("SetTextI18n")
+        protected void onPostExecute(String result){
+            if(result==null) {
+                Toast.makeText(mContext,"Unable to connect to server", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            try {
+
+                JSONObject jsonObject = new JSONObject(result);
+                FollowItem.setTitle("Followers   " + jsonObject.getString("Followers"));
+                BookItem.setTitle("My Books   " + jsonObject.getString("CountBooks"));
+                userName.setText(jsonObject.getString("UserName"));
+                GetUserPicture Pic = new GetUserPicture();
+                Pic.execute(jsonObject.getString("photourl"));
+            }
+            catch(JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
 }
