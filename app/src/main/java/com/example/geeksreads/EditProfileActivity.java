7a1d8d5af9afa17aab3a4a3f50add649bab367af
Mahmoud Morsy <mahmoud_morsy@live.com>
2019-03-27 19:28:34 +0200

    Added Mimic Service to "Set" Profile Details for Edit Profile Activity

--- app/src/main/java/com/example/geeksreads/EditProfileActivity.java
+++ app/src/main/java/com/example/geeksreads/EditProfileActivity.java
@@ -12,7 +12,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SearchView;
 import android.widget.Spinner;
@@ -71,6 +73,81 @@ public class EditProfileActivity extends AppCompatActivity {
         /* Creating a new instance of Sign in Class */
         GetProfileData getProfileData = new GetProfileData();
         getProfileData.execute(urlService,mJSON.toString());
+
+        final EditText fullNameTxt = findViewById(R.id.FullNameTxt);
+        final EditText userNameTxt = findViewById(R.id.UserNameTxt);
+        final EditText emailAddressTxt = findViewById(R.id.EmailAddressTxt);
+        final EditText birthDateTxt = findViewById(R.id.BirthDate);
+        final Spinner countryList = findViewById(R.id.CountryList);
+        final Spinner genderList = findViewById(R.id.GenderList);
+
+
+        Button saveChange = findViewById(R.id.SaveChangesBtn);
+        saveChange.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                String fullNameStr = fullNameTxt.getText().toString();
+                String emailStr = emailAddressTxt.getText().toString();
+                String userNameStr = userNameTxt.getText().toString();
+                String birthDateStr = birthDateTxt.getText().toString();
+                String countryStr = countryList.getSelectedItem().toString();
+                String genderStr = genderList.getSelectedItem().toString();
+
+                fullNameTxt.setError(null);
+                emailAddressTxt.setError(null);
+                userNameTxt.setError(null);
+                birthDateTxt.setError(null);
+                /* If the user entered an invalid Username */
+                if (userNameStr.length() < 3 || userNameStr.length() > 50)
+                {
+                    userNameTxt.setError("Username length should be 3 characters minimum and 50 characters maximum");
+                    //sForTest = "Username length should be 3 characters minimum and 50 characters maximum";
+                }
+                /* If the user entered an invalid Email Address */
+                else if (!emailStr.matches(".+[@].+[.].+"))
+                {
+                    emailAddressTxt.setError("Please enter a valid email");
+                    //sForTest = "Please enter a valid Email";
+                }
+                /* If the user entered an invalid Full Name */
+                else if (fullNameStr.length() < 5 || fullNameStr.length() > 100)
+                {
+                    fullNameTxt.setError("Full name should be 5 characters minimum and 100 characters maximum");
+                    //sForTest = "Please enter a valid Email";
+                }
+                else if (!birthDateStr.matches("[0-9]+[/][0-9]+[/][1][9][0-9][0-9]"))
+                {
+                    birthDateTxt.setError("Please enter a valid date");
+                }
+                /* If the user entered a valid username, email and password */
+                else
+                {
+                    JSONObject JSON = new JSONObject();
+                    try {
+                        JSON.put("UserID", LoginActivity.sCurrentUserID);
+                        JSON.put("UserToken", LoginActivity.sCurrentToken);
+                        JSON.put("FullName", fullNameStr);
+                        JSON.put("UserName", userNameStr);
+                        JSON.put("EmailAddress", emailStr);
+                        JSON.put("Gender", genderStr);
+                        JSON.put("Country", countryStr);
+                        JSON.put("BirthDate", birthDateStr);
+
+                    }catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    /* URL For Sign up API */
+                    String urlService = "http://geeksreads.000webhostapp.com/Morsy/SetProfileDetails.php";
+
+                    /* Creating a new instance of Sign up Class */
+                    SaveProfileData saveProfileData = new SaveProfileData();
+                    saveProfileData.execute(urlService,JSON.toString());
+                }
+            }
+        });
+
     }
 
     @Override
@@ -179,6 +256,108 @@ public class EditProfileActivity extends AppCompatActivity {
                 if (compareValue != null) {
                     int spinnerPosition = adapter.getPosition(compareValue);
                     genderList.setSelection(spinnerPosition);
+                }
+            }
+            /* Catching Exceptions */
+            catch(JSONException e)
+            {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public class SaveProfileData extends AsyncTask<String, Void, String> {
+        JSONObject mJSON = new JSONObject();
+        static final String REQUEST_METHOD="GET";
+
+        @Override
+        protected void onPreExecute() {
+            /* Do Nothing */
+        }
+
+        @Override
+        protected String doInBackground(String... params){
+            String UrlString = params[0];
+            String JSONString = params[1];
+            String result= "";
+
+            try {
+                /* Create a URL object holding our url */
+                URL url = new URL(UrlString);
+                /* Create an HTTP Connection and adjust its options */
+                HttpURLConnection http = (HttpURLConnection) url.openConnection();
+                http.setRequestMethod(REQUEST_METHOD);
+                http.setDoInput(true);
+                http.setDoOutput(true);
+
+                /* A Stream object to hold the sent data to API Call */
+                OutputStream ops = http.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(ops, StandardCharsets.UTF_8));
+                String data = URLEncoder.encode("Json","UTF-8")+"="+URLEncoder.encode(JSONString,"UTF-8");
+
+                writer.write(data);
+                writer.flush();
+                writer.close();
+                ops.close();
+
+                /* A Stream object to get the returned data from API Call */
+                InputStream ips = http.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(ips, StandardCharsets.ISO_8859_1));
+                String line ="";
+                while ((line = reader.readLine()) != null)
+                {
+                    result += line;
+                }
+                reader.close();
+                ips.close();
+                http.disconnect();
+                return result;
+
+            }
+            /* Handling Exceptions */
+            catch (MalformedURLException e) {
+                result = e.getMessage();
+            } catch (IOException e) {
+                result = e.getMessage();
+            }
+            return result;
+        }
+
+        @SuppressLint("SetTextI18n")
+        protected void onPostExecute(String result)
+        {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
+            if(result==null) {
+                Toast.makeText(mContext,"Unable to connect to server", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            try {
+                /* Creating a JSON Object to parse the data in */
+                final JSONObject jsonObject = new JSONObject(result);
+
+                if (jsonObject.getString("ReturnMsg").equals("Your changes are saved successfully"))
+                {
+                    Toast.makeText(mContext,"Your changes are saved successfully", Toast.LENGTH_SHORT).show();
+                }
+                else if (jsonObject.getString("ReturnMsg").contains("A verification email has been sent"))
+                {
+                    dialog.setMessage(jsonObject.getString("ReturnMsg"));
+                    dialog.setPositiveButton("OK", new DialogInterface.OnClickListener()
+                    {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which)
+                        {
+                            /* Go to Next Activity Layout */
+                            Intent myIntent = new Intent(EditProfileActivity.this, MyBooksShelvesActivity.class);
+                            startActivity(myIntent);
+                        }
+                    });
+                    dialog.show();
+                }
+                else
+                {
+                    Toast.makeText(mContext,"An Error Occurred", Toast.LENGTH_SHORT).show();
                 }
             }
             /* Catching Exceptions */
