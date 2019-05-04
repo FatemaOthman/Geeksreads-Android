9eb00a3342dd3d80af7f91c762fdba4f9f85314f
Amr Khaled <amr.soliman97@eng-st.cu.edu.eg>
2019-05-04 12:25:05 +0200

    Changed Json Names in Profile and OtherProfile Activities.

--- app/src/main/java/CustomFunctions/APIs.java
+++ app/src/main/java/CustomFunctions/APIs.java
@@ -9,7 +9,7 @@ public class APIs {
     public static String API_CHANGE_FORGOT_PASSWORD = "https://geeksreads.herokuapp.com/api/users/ChangeForgotPassword";
     public static String API_SIGNOUT = "https://geeksreads.herokuapp.com/api/users/signout";
     public static String API_UPDATE_USER_INFO = "https://geeksreads.herokuapp.com/api/users/update";
-    //    public static String API_GET_USER_INFO = "https://geeksreads.herokuapp.com/api/users/me";
+    public static String API_GET_USER_INFO = "https://geeksreads.herokuapp.com/api/users/me";
     public static String API_UPDATE_USER_PASSWORD = "https://geeksreads.herokuapp.com/api/users/UpdateUserPassword";
     public static String API_GET_SHELVES_COUNT = "https://geeksreads.herokuapp.com/api/users/ShelvesCount";
     public static String API_UPLOAD_PHOTO = "http://geeksreads.000webhostapp.com/Morsy/UploadPhoto.php";
