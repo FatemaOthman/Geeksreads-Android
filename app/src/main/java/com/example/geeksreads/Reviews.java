62270728fe7f1512fa53421988c5f19d9a8ee78f
Amr Khaled <amr.soliman97@eng-st.cu.edu.eg>
2019-05-07 16:03:48 +0200

    Edited: -Added Reviews Rating and Date.

--- app/src/main/java/com/example/geeksreads/Reviews.java
+++ app/src/main/java/com/example/geeksreads/Reviews.java
@@ -49,7 +49,8 @@ public class Reviews extends AppCompatActivity {
         BookNameForReview = findViewById(R.id.ReviewsName);
 
         BookNameForReview.setText(getIntent().getStringExtra("BookName"));
-       /*
+
+        /*
         final String GetAllReviewsURL = APIs.API_GET_REVIEWS_LIST + "?UserId=" + "5ccb84e1496cbb0017f48128" + "&" +
                 "bookId=" + "5c9114526f1439874b7cca1a";
         */
