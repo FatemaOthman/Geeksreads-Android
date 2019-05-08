1718b9cdd887daa870d6ad5c53fe6e4ae19fb5de
Mahmoud Morsy <mahmoud_morsy@live.com>
2019-05-08 08:23:44 +0200

    Updated LoadingView Class to avoid null parameters

--- app/src/main/java/com/example/geeksreads/views/LoadingView.java
+++ app/src/main/java/com/example/geeksreads/views/LoadingView.java
@@ -8,13 +8,13 @@ import android.widget.TextView;
 import com.example.geeksreads.R;
 
 public class LoadingView {
-    TextView[] allControls;
-    FrameLayout FindViewByIdForProgressBarHolder;
-    TextView FindViewByIdFromProgressName;
-    boolean Initialized = false;
-    AlphaAnimation inAnimation;
-    AlphaAnimation outAnimation;
-    FrameLayout progressBarHolder;
+    private TextView[] allControls;
+    private FrameLayout FindViewByIdForProgressBarHolder;
+    private TextView FindViewByIdFromProgressName;
+    private boolean Initialized = false;
+    private AlphaAnimation inAnimation;
+    private AlphaAnimation outAnimation;
+    private FrameLayout progressBarHolder;
 
     public LoadingView(TextView[]ObjList, FrameLayout _FindViewByIdForProgressBarHolder, TextView _FindViewByIdFromProgressName)
     {
@@ -26,9 +26,13 @@ public class LoadingView {
     }
     public void Start(String LoadingText)
     {
-        for (int i=0; i<allControls.length; i++)
+        if (!Initialized) return;
+        if (allControls != null)
         {
-            allControls[i].setEnabled(false);
+            for (int i=0; i<allControls.length; i++)
+            {
+                allControls[i].setEnabled(false);
+            }
         }
         inAnimation = new AlphaAnimation(0f, 1f);
         inAnimation.setDuration(200);
@@ -41,8 +45,11 @@ public class LoadingView {
     public void Stop ()
     {
         if (!Initialized) return;
-        for (int i=0; i < allControls.length; i++) {
-            allControls[i].setEnabled(true);
+        if (allControls != null)
+        {
+            for (int i=0; i < allControls.length; i++) {
+                allControls[i].setEnabled(true);
+            }
         }
         outAnimation=new AlphaAnimation(1f, 0f);
         outAnimation.setDuration(200);
